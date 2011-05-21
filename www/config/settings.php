9370992a9bfe3027fd55d721f7823c20170199d3
giro <jaroslav.semancik@kohovolit.eu>
2011-05-21 22:48:46 +0200

    added config/ directory

---
+++ www/config/settings.php
@@ -0,0 +1,7 @@
+<?php
+
+const API_ROOT = 'home/shared/api.kohovolit.eu/www';
+error_reporting(E_NONE);
+date_default_timezone_set('Europe/Prague');
+
+?>
---
+++ www/projects/kohovolit/config/settings.php
@@ -0,0 +1,6 @@
+<?php
+
+const DATA_DIRECTORY = '/home/shared/kohovolit.eu/www/data';
+const LOGS_DIRECTORY = '/home/shared/kohovolit.eu/logs';
+
+?>
