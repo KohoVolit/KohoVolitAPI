6ec564d093708f3cac9ff939e35923544e5a6a48
michal <michal@sloup.(none)>
2011-05-25 02:17:43 +0200

    for pull only

--- www/config/settings.php
+++ www/config/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-const API_ROOT = 'd:/projekty/KohoVolit.eu/KVG4/api.kohovolit.eu';
+const API_ROOT = '/home/michal/api.kohovolit.eu';
 error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('Europe/Prague');
 
--- www/projects/kohovolit/config/settings.php
+++ www/projects/kohovolit/config/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-const KOHOVOLIT_DIR = '/home/shared/kohovolit.eu';
+const KOHOVOLIT_DIR = '/home/michal/kohovolit.eu';
 define ("DATA_DIR", KOHOVOLIT_DIR . '/www/data');
 define ("LOGS_DIR", KOHOVOLIT_DIR . '/logs');
 
