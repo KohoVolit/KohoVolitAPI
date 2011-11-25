c3552260529818b14b3e801e80dc98d9975d58a0
Jaroslav Semančík <jaroslav_semancik@yahoo.com>
2011-11-25 19:00:10 +0100

    bugfix

--- projects/napistejim/resources/FindMps.php
+++ projects/napistejim/resources/FindMps.php
@@ -3,23 +3,26 @@
 /**
  * \ingroup napistejim
  *
- * Finds MPs that are elected in a given constituency and/or that are members of all given groups at the given moment.
+ * Finds MPs that are elected in a given parliament, constituency and/or that are members of all given groups at the given moment.
  */
 class FindMps
 {
 	/**
-	 * Searches for MPs that are elected in a given constituency and/or that are members of all given groups at the given moment.
+	 * Searches for MPs that are elected in a given parliament, constituency and/or that are members of all given groups at the given moment.
 	 *
 	 * \param $params An array of pairs <em>parameter => value</em> specifying MPs to select. Available parameters are:
-	 * - \c constituency specifying constituency id
+	 * - \c parliament_code specifying parliament code
+	 * - \c constituency_id specifying constituency id
 	 * - \c groups specifying a vertical bar-separated list of groups' ids
 	 * - \c _datetime specifying date and time of the memeberships. If ommitted the current moment is supposed.
+	 *
+	 * At least one of the \c parliament_code, \c constituency_id or \c groups must be specified.
 	 *
 	 * \return An array of MP names.
 	 *
 	 * \ex
 	 * \code
-	 * read(array('constituency' => 10, 'groups' => '576|592'))
+	 * read(array('constituency_id' => 10, 'groups' => '576|592'))
 	 * \endcode returns something like
 	 * \code
 	 * Array
@@ -30,7 +33,7 @@ class FindMps
 	 *             [first_name] => Luděk
 	 *             [middle_names] =>
 	 *             [last_name] => Jeništa
-	 *             [disambiguation] => 
+	 *             [disambiguation] =>
 	 *         )
 	 *
 	 *     [1] => Array
@@ -39,7 +42,7 @@ class FindMps
 	 *             [first_name] => Jan
 	 *             [middle_names] =>
 	 *             [last_name] => Smutný
-	 *             [disambiguation] => 
+	 *             [disambiguation] =>
 	 *         )
 	 *
 	 * )
@@ -47,9 +50,14 @@ class FindMps
 	 */
 	public function read($params)
 	{
-		$groups = isset($params['groups']) ? explode('|', $params['groups']) : null;
-		$constituency = isset($params['constituency']) ? $params['constituency'] : null;
+		$parliament_code = isset($params['parliament_code']) ? $params['parliament_code'] : null;
+		$constituency_id = isset($params['constituency_id']) ? $params['constituency_id'] : null;
+		$groups = isset($params['groups']) && !empty($params['groups']) ? explode('|', $params['groups']) : null;
 		$datetime = isset($params['_datetime']) ? $params['_datetime'] : 'now';
+
+		// at least one criterion must be specified
+		if (empty($parliament_code) && empty($constituency_id) && empty($groups))
+			throw new Exception('At least criterion (parliament code, constituency id or groups) must be specified to find MPs for.', 400);
 
 		// build a query to search for MPs
 		$query = new Query();
@@ -68,10 +76,10 @@ class FindMps
 				"		having count(*) = $3\n" .
 				"	) as all_groups_mp on all_groups_mp.mp_id = mp.id\n"
 			);
-			$query->appendParam('{' . implode(', ', $groups) . '}');
+			$query->appendParam(Db::arrayOfIntegersArgument($groups));
 			$query->appendParam(count($groups));
 		}
-		if (!empty($constituency))
+		if (!empty($constituency_id))
 		{
 			$query->appendQuery(
 			"	join (\n" .
@@ -79,7 +87,19 @@ class FindMps
 			"		where since <= $1 and until > $1 and constituency_id = $" . ($query->getParamsCount() + 1) . "\n" .
 			"	) as const_mp on const_mp.mp_id = mp.id"
 			);
-			$query->appendParam($constituency);
+			$query->appendParam($constituency_id);
+		}
+		if (!empty($parliament_code))
+		{
+			$query->appendQuery(
+			"	join (\n" .
+			"		select distinct mp_id\n" .
+			"		from mp_in_group as mig\n" .
+			"			join \"group\" as g on g.id = mig.group_id\n" .
+			"		where mig.since <= $1 and mig.until > $1 and g.group_kind_code = 'parliament' and g.parliament_code = $" . ($query->getParamsCount() + 1) . "\n" .
+			"	) as parl_mp on parl_mp.mp_id = mp.id"
+			);
+			$query->appendParam($parliament_code);
 		}
 
 		// execute the query
