a56e9e0adc72c3ab18a05c6608d45abe31034917
michalskop <michal.skop@kohovolit.eu>
2012-02-13 02:37:18 +0100

    bugfix

--- data model/scripts/manual/disambiguate.sql
+++ data model/scripts/manual/disambiguate.sql
@@ -3,7 +3,7 @@
 
 create or replace function set_mp_disambiguation(parliament_code varchar, mp_source_code varchar, mp_disambiguation varchar)
 returns void as $$
-	update mp set disambiguation = $3 where id = (select mp_id from mp_attribute where "name" = 'source_code' and "value" = $2 and parl = $1);
+	update mp set disambiguation = $3, last_updated_on = 'now' where id = (select mp_id from mp_attribute where "name" = 'source_code' and "value" = $2 and parl = $1);
 $$ language sql;
 
 -- parliament cz/psp
