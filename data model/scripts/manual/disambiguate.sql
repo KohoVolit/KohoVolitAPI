bf437bdd947f31d41d3f5eca6ca51d4a68edf191
Jaroslav Semančík <jaroslav_semancik@yahoo.com>
2012-12-20 23:57:33 +0100

    added manual disambiguations of new MPs

--- data model/scripts/manual/disambiguate.sql
+++ data model/scripts/manual/disambiguate.sql
@@ -65,6 +65,56 @@ select set_mp_disambiguation('cz/jihomoravsky', 'cz_jihomoravsky_2008-2012_37', 
 select set_mp_disambiguation('cz/nepomuk', 'cz_nepomuk_2010-2014_4', 'Nepomuk 2010-');
 
 
+select set_mp_disambiguation('cz/psp', '346', 'PSP ČR 1998-2002,2010-');
+select set_mp_disambiguation('cz/humpolec', 'cz_humpolec_2010-2014_4', 'Humpolec 2010 - 2014');
+
+select set_mp_disambiguation('cz/vysocina', 'cz_vysocina_2008-2012_9', 'Kraj Vysočina 2008 - 2012');
+select set_mp_disambiguation('cz/moravske-budejovice', 'cz_moravske-budejovice_2010-2014_5', 'Moravské Budějovice 2010 - 2014');
+
+select set_mp_disambiguation('cz/cesky-krumlov', 'cz_cesky-krumlov_2010-2014_16', 'Český Krumlov 2010 - 2014');
+select set_mp_disambiguation('cz/hradec-kralove', 'cz_hradec-kralove_2010-2014_2', 'Hradec Králové 2010 - 2014');
+
+select set_mp_disambiguation('cz/olomoucky', 'cz_olomoucky_2008-2012_4', 'Olomoucký kraj 2008 - 2012');
+select set_mp_disambiguation('cz/humpolec', 'cz_humpolec_2010-2014_12', 'Humpolec 2010 - 2014');
+
+select set_mp_disambiguation('cz/ustecky', 'cz_ustecky_2008-2012_41', 'Ústecký kraj 2008 - 2012');
+select set_mp_disambiguation('cz/pardubicky', 'cz_pardubicky_2008-2012_4', 'Pardubický kraj 2008 - 2012');
+select set_mp_disambiguation('cz/boskovice', 'cz_boskovice_2010-2014_2', 'Boskovice 2010 - 2014');
+
+select set_mp_disambiguation('cz/psp', '5401', 'PSP ČR 2002-2006');
+select set_mp_disambiguation('cz/jablonec-nad-nisou', 'cz_jablonec-nad-nisou_2010-2014_20', 'Jablonec nad Nisou 2010 - 2014');
+
+select set_mp_disambiguation('cz/jihlava', 'cz_jihlava_2010-2014_12', 'Jihlava 2010 - 2014');
+select set_mp_disambiguation('cz/pelhrimov', 'cz_pelhrimov_2010-2014_4', 'Pelhřimov 2010 - 2014');
+
+select set_mp_disambiguation('cz/tanvald', 'cz_tanvald_2010-2014_10', 'Tanvald 2010 - 2014 (10)');
+select set_mp_disambiguation('cz/tanvald', 'cz_tanvald_2010-2014_11', 'Tanvald 2010 - 2014 (11)');
+
+select set_mp_disambiguation('cz/psp', '382', 'PSP ČR 1998-2006');
+select set_mp_disambiguation('cz/bucovice', 'cz_bucovice_2010-2014_15', 'Bučovice 2010 - 2014');
+
+select set_mp_disambiguation('cz/jihomoravsky', 'cz_jihomoravsky_2008-2012_31', 'Jihomoravský kraj 2008 - 2012');
+select set_mp_disambiguation('cz/namest-nad-oslavou', 'cz_namest-nad-oslavou_2010-2014_15', 'Náměšť nad Oslavou 2010 - 2014');
+
+select set_mp_disambiguation('cz/uhersky-brod', 'cz_uhersky-brod_2010-2014_21', 'Uherský Brod 2010 - 2014');
+select set_mp_disambiguation('cz/nove-mesto-na-morave', 'cz_nove-mesto-na-morave_2010-2014_17', 'Nové Město na Moravě 2010 - 2014');
+
+select set_mp_disambiguation('cz/jilemnice', 'cz_jilemnice_2010-2014_9', 'Jilemnice 2010 - 2014');
+select set_mp_disambiguation('cz/turnov', 'cz_turnov_2010-2014_18', 'Turnov 2010 - 2014');
+
+select set_mp_disambiguation('cz/usti-nad-labem', 'cz_usti-nad-labem_2010-2014_17', 'Ústí nad Labem 2010 - 2014');
+select set_mp_disambiguation('cz/trebic', 'cz_trebic_2010-2014_19', 'Třebíč 2010 - 2014');
+
+select set_mp_disambiguation('cz/trebic', 'cz_trebic_2010-2014_20', 'Třebíč 2010 - 2014');
+select set_mp_disambiguation('cz/jablonec-nad-nisou', 'cz_jablonec-nad-nisou_2010-2014_14', 'Jablonec nad Nisou 2010 - 2014');
+
+select set_mp_disambiguation('cz/karlovarsky', 'cz_karlovarsky_2008-2012_8', 'Karlovarský kraj 2008 - 2012');
+select set_mp_disambiguation('cz/tanvald', 'cz_tanvald_2010-2014_7', 'Tanvald 2010 - 2014');
+
+select set_mp_disambiguation('cz/usti-nad-labem', 'cz_usti-nad-labem_2010-2014_18', 'Ústí nad Labem 2010 - 2014');
+select set_mp_disambiguation('cz/tanvald', 'cz_tanvald_2010-2014_22', 'Tanvald 2010 - 2014');
+
+
 -- parliament sk/starostovia
 select set_mp_disambiguation('sk/starostovia', '514349-Stanislav-Bartoš', 'Radobica, starosta 2010-');
 select set_mp_disambiguation('sk/starostovia', '525260-Stanislav-Bartoš', 'Široké, starosta 2010-');
